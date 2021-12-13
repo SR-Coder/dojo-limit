e7ce53627604a9ee9eeb405a171b9de3eb2ecbda
saconverse <shawn.a.converse@outlook.com>
2021-12-13 15:07:15 -0800

    changed StacksToKeep to an object to increase efficiency with lookup. put main DOM manipulation code inside of chrome.storage.sync to reduce number of calls.

--- background.js
+++ background.js
@@ -1,13 +1,26 @@
-console.log('running everyt time background');
+
 let stackList = [];
-let StacksToKeep = [
-    'PROGRAMMING BASICS - 2021 ',
-    'WEB FUNDAMENTALS (2021) ',
-    'PYTHON v21.1 ',
-    'C# / .NET Core ',
-    'MERN ',
-    'Java '
-]
+// let StacksToKeep = [
+//     'PROGRAMMING BASICS - 2021 ',
+//     'WEB FUNDAMENTALS (2021) ',
+//     'PYTHON v21.1 ',
+//     'C# / .NET Core ',
+//     'MERN ',
+//     'JAVA '
+// ]
+
+let StacksToKeep = {
+    'PROGRAMMING BASICS - 2021 ': true,
+    'WEB FUNDAMENTALS (2021) ': true,
+    'PYTHON V21.1 ': true,
+    'C# / .NET CORE ': true,
+    'MERN ': true,
+    'JAVA ': true,
+    'CAREER SERVICES ': true,
+    'LAMP ': true,
+    "HOW TO CREATE A COURSE ": true,
+    "PRE-BOOTCAMP ONLINE ": true
+}
 
 chrome.runtime.onInstalled.addListener(()=>{
     chrome.storage.sync.set({stackList});
